5459b83cbf6a258cddbe37192dae9e9935ee398e
Ankit Gupta <happyankitgupta@gmail.com>
2021-06-16 09:59:57 +0530

    Adding Intial Section Of Routing

--- 11) Changing Pages with Routing/routing/src/app/app.module.ts
+++ 11) Changing Pages with Routing/routing/src/app/app.module.ts
@@ -1,40 +1,3 @@
-### 123) Module Introduction :
-
-### 124) Why do we need a Router? :
-
-### 125) Understanding the Example Project :
-- In our app, we got three sections:
-    - Home
-    - Servers
-        - View and Edit Servers
-        - A Service is used to load and update Servers
-    - Users
-        - View Users
-- This app will be improved by adding routing but definitely feel free to play around with it - besides routing, everything should be working fine.
-
-### 126) Setting up and Loading Routes :
-- router-outlet is a directive.
-- app.component.html
-```
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
-      <ul class="nav nav-tabs">
-        <li role="presentation" class="active"><a href="#">Home</a></li>
-        <li role="presentation"><a href="#">Servers</a></li>
-        <li role="presentation"><a href="#">Users</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
-      <router-outlet></router-outlet>
-    </div>
-  </div>
-</div>
-```
-- app.module.ts
-```
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -74,6 +37,3 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-```
-
-### 126) Navigating with Router Links :
